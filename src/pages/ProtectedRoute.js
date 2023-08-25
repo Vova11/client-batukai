b94e0aa7f6a3665cf84e01796280485146e5f0e3
vova11 <vladimir.zembera@gmail.com>
2023-08-25 13:32:09 +0200

    new protected route

--- src/pages/ProtectedRoute.js
+++ src/pages/ProtectedRoute.js
@@ -2,12 +2,13 @@ import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchUser, getUser, updateUser } from '../features/user/userSlice';
-
 import Spinner from '../components/Dashboard/Spinner';
+
 const ProtectedRoute = ({ children }) => {
 	const { user, isLoading, userObject } = useSelector((store) => store.user);
 	const dispatch = useDispatch();
 	const navigate = useNavigate();
+
 	useEffect(() => {
 		if (!user && !isLoading) {
 			dispatch(fetchUser());
@@ -18,7 +19,7 @@ const ProtectedRoute = ({ children }) => {
 	}, [user, isLoading, userObject, dispatch]);
 
 	if (isLoading) {
-		return <Spinner />;
+		return <Spinner />; // Show a loading indicator while user authentication is being checked
 	}
 
 	if (!user) {
@@ -26,10 +27,53 @@ const ProtectedRoute = ({ children }) => {
 		return null; // Return null to prevent rendering the protected component when user is not logged in
 	}
 
-	return children;
+	return children; // Render the protected content if user is authenticated
 };
 
 export default ProtectedRoute;
+
+//----------------
+//----------------
+//----------------
+//----------------
+
+// import React, { useEffect } from 'react';
+// import { useNavigate } from 'react-router-dom';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { fetchUser, getUser, updateUser } from '../features/user/userSlice';
+
+// import Spinner from '../components/Dashboard/Spinner';
+// const ProtectedRoute = ({ children }) => {
+// 	const { user, isLoading, userObject } = useSelector((store) => store.user);
+// 	const dispatch = useDispatch();
+// 	const navigate = useNavigate();
+// 	useEffect(() => {
+// 		if (!user && !isLoading) {
+// 			dispatch(fetchUser());
+// 		}
+// 		if (Object.keys(userObject).length === 0 && user) {
+// 			dispatch(getUser(user.id));
+// 		}
+// 	}, [user, isLoading, userObject, dispatch]);
+
+// 	if (isLoading) {
+// 		return <Spinner />;
+// 	}
+
+// 	if (!user) {
+// 		navigate('/login');
+// 		return null; // Return null to prevent rendering the protected component when user is not logged in
+// 	}
+
+// 	return children;
+// };
+
+// export default ProtectedRoute;
+
+//----------------
+//----------------
+//----------------
+//----------------
 
 // import React, { useEffect } from 'react';
 // import { useNavigate } from 'react-router-dom';
