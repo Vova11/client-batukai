cb51eb50438d002fe10fb67ebf9f4abd1639553e
vova11 <vladimir.zembera@gmail.com>
2023-08-28 10:24:24 +0200

    check redirect

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -5,7 +5,6 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { PageHero, OrderForm } from './';
-import { Link } from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
 import { formatPrice } from '../utils/helpers';
 const Checkout = () => {
